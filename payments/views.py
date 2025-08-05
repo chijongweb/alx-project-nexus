ef7f89710640086dccab301acb2aab87a3594e1c
chijongweb <bchijongwe1@gmail.com>
2025-08-05 16:02:50 +0200

    payments update

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Payment
+from .serializers import PaymentSerializer
 
-# Create your views here.
+class PaymentListCreateView(generics.ListCreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class PaymentDetailView(generics.RetrieveAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    lookup_field = 'payment_id'
