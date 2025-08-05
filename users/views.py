7d11d212b3e37cec25df205a71fb73046b83f36e
chijongweb <bchijongwe1@gmail.com>
2025-08-05 15:17:02 +0200

    user view update

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
-from django.contrib.auth.models import User  
+from django.contrib.auth import get_user_model
 from rest_framework.permissions import AllowAny
 from rest_framework.serializers import ModelSerializer
+
+User = get_user_model()
 
 class RegisterSerializer(ModelSerializer):
     class Meta:
