22128f5a71c4f402acd7d022451dec378763c59f
chijongweb <bchijongwe1@gmail.com>
2025-08-07 05:28:59 +0200

    update users/url

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, CustomLoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomLoginView.as_view(), name='token_obtain_pair'),
 ]
+
+
+
+
