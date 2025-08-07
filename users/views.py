e65613b062ffe04fac5efe86a85cd96bf6e2bb45
chijongweb <bchijongwe1@gmail.com>
2025-08-07 05:23:23 +0200

    users/view update

--- users/views.py
+++ users/views.py
@@ -4,6 +4,10 @@ from rest_framework.permissions import AllowAny
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 
 User = get_user_model()
 
@@ -20,6 +24,34 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+class CustomLoginView(TokenObtainPairView):
+    @swagger_auto_schema(
+        operation_description="Log in to obtain JWT token",
+        tags=["Auth"],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'username': openapi.Schema(type=openapi.TYPE_STRING, description='Your username'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='Your password'),
+            },
+            required=['username', 'password']
+        ),
+        responses={
+            200: openapi.Response(
+                description="JWT Token",
+                examples={
+                    "application/json": {
+                        "access": "your-access-token",
+                        "refresh": "your-refresh-token"
+                    }
+                }
+            ),
+            401: "Unauthorized"
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
 
 
