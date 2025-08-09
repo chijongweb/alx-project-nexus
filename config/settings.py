5403fe88d9b1a30d0b185b97f544cb1277eecdd2
chijongweb <bchijongwe1@gmail.com>
2025-08-09 10:18:54 +0200

    update environment settings

--- config/settings.py
+++ config/settings.py
@@ -23,15 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-yikjy$ac5&(h=et=#wv=3--)0&*jal+i99ov8#wk&c8gd5m-(v'
 SECRET_KEY = config('SECRET_KEY')
-
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = config('DEBUG', cast=bool)
+#DEBUG = config('DEBUG', cast=bool)
 
-if DEBUG:
-    ALLOWED_HOSTS = []
-else:
-    ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+#if DEBUG:
+ #   ALLOWED_HOSTS = []
+#else:
+  #  ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 
 #ALLOWED_HOSTS = []
 
